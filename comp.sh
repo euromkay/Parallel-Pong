b496175b1a8b42313af9c0e98e37b3ac6f4e45a5
Charles <euromkay@gmail.com>
2015-08-04 17:24:36 -0700

    More script attempts

---
+++ comp.sh
@@ -0,0 +1,5 @@
+export DISPLAY=:0
+
+cd shared/Parallel-Pong/
+python pong.py &
+exit
--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-0-0
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-0-1
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-0-2
+cat comp.sh | ssh tile-0-0
+cat comp.sh | ssh tile-0-1
+cat comp.sh | ssh tile-0-2
 
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-1-0
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-1-1
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-1-2
+cat comp.sh | ssh tile-1-0
+cat comp.sh | ssh tile-1-1
+cat comp.sh | ssh tile-1-2
 
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-2-0
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-2-1
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-2-2
+cat comp.sh | ssh tile-2-0
+cat comp.sh | ssh tile-2-1
+cat comp.sh | ssh tile-2-2
 
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-3-0
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-3-1
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-3-2
+cat comp.sh | ssh tile-3-0
+cat comp.sh | ssh tile-3-1
+cat comp.sh | ssh tile-3-2
 
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-4-0
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-4-1
-echo "cd shared/Parallel-Pong/; DISPLAY=:0.0 python pong.py & exit" | ssh tile-4-2
+cat comp.sh | ssh tile-4-0
+cat comp.sh | ssh tile-4-1
+cat comp.sh | ssh tile-4-2
